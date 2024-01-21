9aa26c446cc3e1473076b74646125f95fe78b3b1
bigbullpaisewala <bigbullpaisewala@gmail.com>
2024-01-21 14:23:49 +0530

    done

--- middlewares/authenication.js
+++ middlewares/authenication.js
@@ -4,8 +4,6 @@ const jwt = require("jsonwebtoken");
 
 const authentication = (req, res, next) => {
     const token = req.headers.authorization?.split(" ")[1]  //optional chaining
-
-    
     if (!token) {
         return res.send({
             msg: 'please login first as user'
