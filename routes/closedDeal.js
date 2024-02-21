e6c3cab4a5b20bea774b221e0fdeacccf2a4aee5
bigbullpaisewala <bigbullpaisewala@gmail.com>
2024-02-21 10:30:52 +0530

    done

--- routes/closedDeal.js
+++ routes/closedDeal.js
@@ -4,23 +4,32 @@ const DealModel = require("../models/Deal");
 const ClosedDealModel = require("../models/ClosedDeal");
 const DepositModel = require("../models/adminDeposit.Model");
 const { userModel } = require("../models/UserModel");
-const authentication=require("../middlewares/authenication")
+const authentication = require("../middlewares/authenication")
 
+
+const dealClosingLocks = {};
 ClosedDealRoute.post('/:id/:order_id', async (req, res) => {
+    // Assuming userId is provided in the request body
+    const { close_rate, manual_auto, order_profit, closed_at } = req.body
+    // Fetch data from DealModel based on userId
+    const { id, order_id } = req.params;
+    if (dealClosingLocks[`${id}_${order_id}`]) {
+        return res.status(409).json({ msg: "Deals are already being closed for this dealer_id and order_id. Try again later." });
+    }
+
+    // Acquire the lock
+    dealClosingLocks[`${id}_${order_id}`] = true;
     try {
-        // Assuming userId is provided in the request body
-         const {close_rate,manual_auto,order_profit,closed_at}=req.body
-        // Fetch data from DealModel based on userId
-        const { id, order_id } = req.params;
+
 
         // Fetch data from DealModel based on userId and order_id
         const deals = await DealModel.find({ dealer_id: id, order_id: order_id });
-           
+
         // Use the fetched data to create entries in the ClosedDealModel
         const closedDeals = await ClosedDealModel.create(deals.map(deal => ({
             // Map the fields accordingly based on your ClosedDeal model structure
             dealer_id: deal.dealer_id,
-            order_id:deal.order_id,
+            order_id: deal.order_id,
             order_type: deal.order_type,
             title: deal.title,
             lotsize: deal.lotsize,
@@ -33,8 +42,50 @@ ClosedDealRoute.post('/:id/:order_id', async (req, res) => {
             manual_auto,
             closed_at
         })));
-       
+
         await DealModel.deleteMany({ dealer_id: id, order_id: order_id });
+        const firstClosedDeal = closedDeals.length > 0 ? closedDeals[0] : {};
+        const { order_profit: addedOrderProfit } = firstClosedDeal;
+        return res.status(200).json({ msg: "Closed deals added successfully", order_profit: addedOrderProfit });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ msg: "Error in processing closed deals" });
+    }
+    finally {
+        // Release the lock
+        dealClosingLocks[`${id}_${order_id}`] = false;
+    }
+});
+
+ClosedDealRoute.post('/:id', async (req, res) => {
+    try {
+        // Assuming userId is provided in the request body
+        const { close_rate, manual_auto, order_profit, closed_at } = req.body
+        // Fetch data from DealModel based on userId
+        const { id } = req.params;
+
+        // Fetch data from DealModel based on userId and order_id
+        const deals = await DealModel.find({ dealer_id: id });
+
+        // Use the fetched data to create entries in the ClosedDealModel
+        const closedDeals = await ClosedDealModel.create(deals.map(deal => ({
+            // Map the fields accordingly based on your ClosedDeal model structure
+            dealer_id: deal.dealer_id,
+            order_id: deal.order_id,
+            order_type: deal.order_type,
+            title: deal.title,
+            lotsize: deal.lotsize,
+            created_at: deal.created_at,
+            bidorask: deal.bidorask,
+            takeprofit: deal.takeprofit,
+            stoploss: deal.stoploss,
+            order_profit,
+            close_rate,
+            manual_auto,
+            closed_at
+        })));
+
+        await DealModel.deleteMany({ dealer_id: id });
         const firstClosedDeal = closedDeals.length > 0 ? closedDeals[0] : {};
         const { order_profit: addedOrderProfit } = firstClosedDeal;
         return res.status(200).json({ msg: "Closed deals added successfully", order_profit: addedOrderProfit });
@@ -57,7 +108,7 @@ ClosedDealRoute.put("/addprofit/:id", async (req, res) => {
         if (user) {
             // Update the totalbalance in userModel
             user.totalbalance += parseFloat(profitToAdd);
-            user.neteq+=parseFloat(profitToAdd);
+            user.neteq += parseFloat(profitToAdd);
 
             // Save the updated user record
             await user.save();
@@ -76,7 +127,7 @@ ClosedDealRoute.put("/addprofit/:id", async (req, res) => {
             return res.status(200).send({
                 AccountNo: accountId,
                 totalBalance: user.totalbalance, // Updated totalbalance from userModel
-                neteq:user.neteq
+                neteq: user.neteq
             });
         } else {
             return res.status(404).send({ msg: "No user found for the provided account number" });
@@ -89,17 +140,17 @@ ClosedDealRoute.put("/addprofit/:id", async (req, res) => {
 
 ClosedDealRoute.use(authentication);
 
-ClosedDealRoute.get("/:id",async(req,res)=>{
+ClosedDealRoute.get("/:id", async (req, res) => {
     try {
-        const {id}=req.params;
+        const { id } = req.params;
 
-        const deals=await ClosedDealModel.find({dealer_id:id})
+        const deals = await ClosedDealModel.find({ dealer_id: id })
         return res.status(200).send(deals)
     } catch (error) {
         console.log(error)
-        return res.status(500).json({msg:"Error while fetching deals"})
+        return res.status(500).json({ msg: "Error while fetching deals" })
     }
-   
+
 
 })
 
