eb116b4993f6a690525f8245a4d671c6154790a6
bigbullpaisewala <bigbullpaisewala@gmail.com>
2024-02-22 07:14:24 +0100

    done

--- routes/Login.user.js
+++ routes/Login.user.js
@@ -2,11 +2,13 @@ const express = require('express');
 const LoginVRoutes = express.Router();
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
 
 
 const { userModel } = require('../models/UserModel');
 const Token = require('../models/token');
 const AdminModel = require('../models/Admin.Model');
+const sendEmail = require('../utils/sendEmail');
 
 
 LoginVRoutes.post('/', async (req, res) => {
@@ -14,44 +16,112 @@ LoginVRoutes.post('/', async (req, res) => {
         const { email, password } = req.body;
 
         // Check if the email exists in either vendorModel or userModel
-
         const user = await userModel.findOne({ email });
         const admin = await AdminModel.findOne({ email });
-        const model = user || admin
+        const model = user || admin;
+
+        if (!model) {
+            return res.status(400).json({
+                msg: 'User not found with the provided email',
+            });
+        }
+
         const hashed_password = model.password;
         const result = await bcrypt.compare(password, hashed_password);
-        if (!user) {
-            let token = await Token.findOne({ userId: user._id });
-            if (!token) {
-                token = await new Token({
-                    userId: user._id,
-                    token: crypto.randomBytes(32).toString("hex"),
-                }).save();
-                const url = `${process.env.BASE_URL}/users/${user.id}/verify/${token.token}`;
-                await sendEmail(user.email, "Verify Email", url);
-            }
-        }
-        if (result) {
-            const token = jwt.sign({ _id: model._id, userType: user ? user:admin}, process.env.SECRET_KEY);
 
-            return res.status(200).json({
-                msg: `Login successfully `,
-                type: model,
-                token: token,
-
-            });
-        } else {
+        if (!result) {
             return res.status(400).json({
-                msg: 'Wrong password, Please try again later'
+                msg: 'Wrong password, please try again later',
             });
         }
+
+        let userId = null;
+
+        if (model._id) {
+            userId = model._id;
+        } else if (model.id) {
+            userId = model.id;
+        }
+
+        if (!userId) {
+            return res.status(500).json({
+                msg: 'Error getting user ID',
+            });
+        }
+
+        let token = await Token.findOne({ userId });
+
+        if (!token) {
+            token = await new Token({
+                userId,
+                token: crypto.randomBytes(32).toString("hex"),
+            }).save();
+            const url = `${process.env.BASE_URL}/users/${userId}/verify/${token.token}`;
+            await sendEmail(model.email, "Verify Email", url);
+        }
+
+        const userType = user ? user : admin;
+        let toke = jwt.sign({ _id: userId, userType }, process.env.SECRET_KEY);
+
+        return res.status(200).json({
+            msg: 'Login successfully',
+            type: userType,
+            token: toke,
+        });
+
     } catch (error) {
         console.error(error);
         return res.status(500).json({
-            msg: "Login failed, Invalid credentials"
+            msg: 'Login failed, Invalid credentials',
         });
     }
 });
+
+
+// LoginVRoutes.post('/', async (req, res) => {
+//     try {
+//         const { email, password } = req.body;
+
+//         // Check if the email exists in either vendorModel or userModel
+
+//         const user = await userModel.findOne({ email });
+//         const admin = await AdminModel.findOne({ email });
+//         const model = user || admin
+//         const hashed_password = model.password;
+//         const result = await bcrypt.compare(password, hashed_password);
+//         if (!user) {
+//             let token = await Token.findOne({ userId: user._id });
+//             if (!token) {
+//                 token = await new Token({
+//                     userId: user._id,
+//                     token: crypto.randomBytes(32).toString("hex"),
+//                 }).save();
+//                 const url = `${process.env.BASE_URL}/users/${user.id}/verify/${token.token}`;
+//                 await sendEmail(user.email, "Verify Email", url);
+//             }
+//         }
+//         if (result) {
+//             const userType = user ? 'user' : 'admin';
+//             const token = jwt.sign({ _id: model._id, userType}, process.env.SECRET_KEY);
+
+//             return res.status(200).json({
+//                 msg: `Login successfully `,
+//                 type: model,
+//                 token: token,
+
+//             });
+//         } else {
+//             return res.status(400).json({
+//                 msg: 'Wrong password, Please try again later'
+//             });
+//         }
+//     } catch (error) {
+//         console.error(error);
+//         return res.status(500).json({
+//             msg: "Login failed, Invalid credentials"
+//         });
+//     }
+// });
 
 
 LoginVRoutes.post('/user', async (req, res) => {
