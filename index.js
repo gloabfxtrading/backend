4c1915731e71a7128ea49c693d99b51a724dbdc0
bigbullpaisewala <bigbullpaisewala@gmail.com>
2024-02-26 21:56:30 +0530

    done

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const cookie=require("cookie-parser")
 
 const cors = require('cors');
 app.use(cors({
-  origin: '*',
+  origin: 'http://trader.gloabfx.com',
   methods: ["POST", "GET", "PUT", "DELETE"],
   credentials: true
 }));
