36d3959b2f7a4ec3b651956085a7703da15dd960
bigbullpaisewala <bigbullpaisewala@gmail.com>
2024-03-11 12:15:08 +0530

    done

--- routes/Login.user.js
+++ routes/Login.user.js
@@ -3,7 +3,7 @@ const LoginVRoutes = express.Router();
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const crypto = require('crypto');
-const cookie=require("cookie-parser")
+const cookie = require("cookie-parser")
 
 
 const { userModel } = require('../models/UserModel');
@@ -12,17 +12,17 @@ const AdminModel = require('../models/Admin.Model');
 const sendEmail = require('../utils/sendEmail');
 const token = require('../models/token');
 
-const verifyUser=(req,res)=>{
-    const token=req.cookies.token;
-    if(!token){
-        return res.json({msg:"we need token provide it."})
-    }else{
-        jwt.verify(token,process.env.SECRET_KEY,(err,decoded)=>{
-            if(err){
-                return res.json({msg:"Authentication error"})
-            }else{
-                req._id=decoded._id
-                return res.json({Status:"Success",_id:req._id})
+const verifyUser = (req, res) => {
+    const token = req.cookies.token;
+    if (!token) {
+        return res.json({ msg: "we need token provide it." })
+    } else {
+        jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
+            if (err) {
+                return res.json({ msg: "Authentication error" })
+            } else {
+                req._id = decoded._id
+                return res.json({ Status: "Success", _id: req._id })
             }
         })
     }
@@ -36,7 +36,7 @@ LoginVRoutes.post('/', async (req, res) => {
         const user = await userModel.findOne({ email });
         const admin = await AdminModel.findOne({ email });
         const model = user || admin;
-
+        
         if (!model) {
             return res.status(400).json({
                 msg: 'User not found with the provided email',
@@ -46,13 +46,13 @@ LoginVRoutes.post('/', async (req, res) => {
         const hashed_password = model.password;
         const result = await bcrypt.compare(password, hashed_password);
 
-        
+
         if (!result) {
             return res.status(400).json({
                 msg: 'Wrong password, please try again later',
             });
         }
-
+        console.log(model.verified)
         let userId = null;
 
         if (model._id) {
@@ -68,25 +68,25 @@ LoginVRoutes.post('/', async (req, res) => {
         }
 
         let Token = await token.findOne({ userId });
-
-        if (user.verified===false) {
+        
+        if (model.verified === false) {
             Token = await new token({
                 userId,
                 token: crypto.randomBytes(32).toString("hex"),
             }).save();
             const url = `${process.env.BASE_URL}/users/${userId}/verify/${Token.token}`;
             await sendEmail(model.email, "Verify Email", url);
-        }else{
+        } else {
 
-        const userType = user ? user : admin;
-        let toke = jwt.sign({ _id: userId, userType }, process.env.SECRET_KEY);
-        res.cookie("token", toke)
-        return res.status(200).json({
-            msg: 'Login successfully',
-            type: userType,
-            token: toke,
-        });
-    }
+            const userType = user ? user : admin;
+            let toke = jwt.sign({ _id: userId, userType }, process.env.SECRET_KEY);
+            res.cookie("token", toke)
+            return res.status(200).json({
+                msg: 'Login successfully',
+                type: userType,
+                token: toke,
+            });
+        }
 
     } catch (error) {
         console.error(error);
